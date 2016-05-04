a39fcf5b1bc363a7ba98f75f39a1c3091f526aa3
iainscott-rgu <odyssey3007@hotmail.com>
2016-05-04 11:19:08 +0100

    assessed lab

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_SERVER', 'eu-cdbr-azure-north-e.cloudapp.net');
+define('DB_USERNAME', 'be97193a7479f3');
+define('DB_PASSWORD', '270a0140');
+define('DB_DATABASE', 'CMM007AL-9540730');
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
+
