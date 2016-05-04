141b7a01f48b5f301c01a7bfbb054e3e85693fb3
iainscott-rgu <odyssey3007@hotmail.com>
2016-05-04 11:36:54 +0100

    assessed lab

--- blog.php
+++ blog.php
@@ -1,7 +1,88 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: 9540730
- * Date: 04/05/2016
- * Time: 11:26
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>myBlog</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href='https://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="assets/css/unsemantic-grid-responsive-tablet.css">
+
+
+
+</head>
+<body>
+<header>
+    <h1>myBlog</h1><br>
+    <h4>because the internet needs to know what I think</h4>
+
+
+
+    <nav>
+        <ul>
+            <li><a href="blog.php">All Blog Items</a></li>
+            <li><a href="blog.php">Work Items</a></li>
+            <li><a href="blog.php">University Items</a></li>
+            <li><a href="blog.php">Family Items</a></li>
+            <li><a href="add.php">Insert a Blog Items</a></li>
+        </ul>
+    </nav>
+
+
+</header>
+<main class="grid-container">
+
+    <section class='grid-100' id='listcon1'>
+
+    </section>
+    <section class='grid-10' id='listcon2'>
+        Title:
+    </section>
+    <section class='grid-90' id='listcon3'>
+        Today at work by Adam
+    </section>
+    <section class='grid-10' id='listcon4'>
+        Category:
+    </section>
+    <section class='grid-90' id='listcon5'>
+        Work
+    </section>
+    <section class='grid-10' id='listcon6'>
+        Summary:
+    </section>
+    <section class='grid-90' id='listcon7'>
+        Today I went to work and did lots of very complicated coding things. I was very pleased that I managed to finish them all
+    </section>
+
+    <section class='grid-100' id='listcon8'>
+
+    </section>
+
+    <section class='grid-10' id='listcon9'>
+        Title:
+    </section>
+    <section class='grid-90' id='listcon10'>
+        University Lecturer by Brian
+    </section>
+    <section class='grid-10' id='listcon11'>
+        Category:
+    </section>
+    <section class='grid-90' id='listcon12'>
+        University
+    </section>
+    <section class='grid-10' id='listcon13'>
+        Summary:
+    </section>
+    <section class='grid-90' id='listcon14'>
+        This week in university I had an amazing lecture. I can’t remember the name of the lecturer but he was really really good.
+    </section>
+
+
+
+</main>
+<footer>
+    <p>Designed by Iain Scott, 2016</p>
+
+</footer>
+
+</body>
+</html>
