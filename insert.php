36c3d95b0f09b523cee2783eaf70dcb9415f35b5
iainscott-rgu <odyssey3007@hotmail.com>
2016-05-04 12:04:58 +0100

    assessed lab

--- insert.php
+++ insert.php
@@ -1,12 +1,12 @@
 <?php
 include("connect.php");
 $title = $_POST["title"];
-$summary = $_POST["Summary"];
+$summary = $_POST["summary"];
 $category = $_POST["category"];
 $submitter = $_POST["submitter"];
 
 
-$sql = "INSERT INTO blogView (entryTitle, entrySummary, category, submitter) VALUES ('$title','$summary','$category', '$submitter')";
+$sql = "INSERT INTO blogView (entryTitle, entrySummary, category, submitter) VALUES ('$title','$summary','$category','$submitter')";
 if(mysqli_query($db, $sql)) {
 }else{
     echo "Error: " . $sql . "<br>" . mysqli_error($db);
